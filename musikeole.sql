0f74d21748a8415c04a6eeb06fbea2afb6d7d53f
alexandrewavelet <wavelet.alexandre@gmail.com>
2014-03-20 12:19:56 +0100

    BDD
    ajout du script SQL pour la BDD. Il manque encore les tables pour la
    boutique et la bourse au matériel musical

---
+++ musikeole.sql
@@ -0,0 +1,376 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.3
+-- http://www.phpmyadmin.net
+--
+-- Client: 127.0.0.1
+-- Généré le: Jeu 20 Mars 2014 à 12:17
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.5.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `musikeole`
+--
+CREATE DATABASE IF NOT EXISTS `musikeole` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `musikeole`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adherents`
+--
+
+CREATE TABLE IF NOT EXISTS `adherents` (
+  `id` varchar(20) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `telephone` varchar(10) NOT NULL,
+  `idFamille` int(11) NOT NULL,
+  `idMembre` int(11) NOT NULL,
+  `valide` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adherentsassociations`
+--
+
+CREATE TABLE IF NOT EXISTS `adherentsassociations` (
+  `idAdherent` int(11) NOT NULL,
+  `idAssociation` int(11) NOT NULL,
+  PRIMARY KEY (`idAdherent`,`idAssociation`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `albums`
+--
+
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `associations`
+--
+
+CREATE TABLE IF NOT EXISTS `associations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `autorisations`
+--
+
+CREATE TABLE IF NOT EXISTS `autorisations` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `autorisations`
+--
+
+INSERT INTO `autorisations` (`id`, `nom`) VALUES
+(1, 'membre'),
+(2, 'adhérent'),
+(3, 'administrateur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categoriesforum`
+--
+
+CREATE TABLE IF NOT EXISTS `categoriesforum` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `demandesphotos`
+--
+
+CREATE TABLE IF NOT EXISTS `demandesphotos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `valide` tinyint(1) NOT NULL,
+  `lien` varchar(100) NOT NULL,
+  `idAdherent` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `familles`
+--
+
+CREATE TABLE IF NOT EXISTS `familles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscriptionsmanifestations`
+--
+
+CREATE TABLE IF NOT EXISTS `inscriptionsmanifestations` (
+  `idInscription` int(11) NOT NULL AUTO_INCREMENT,
+  `placesAdherents` int(11) NOT NULL,
+  `placesEnfants` int(11) NOT NULL,
+  `placesExterieurs` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `valide` tinyint(1) NOT NULL,
+  `idAdherent` int(11) NOT NULL,
+  `idManifestation` int(11) NOT NULL,
+  PRIMARY KEY (`idInscription`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscritsnewsletter`
+--
+
+CREATE TABLE IF NOT EXISTS `inscritsnewsletter` (
+  `mail` varchar(50) NOT NULL,
+  `idMembre` int(11) NOT NULL,
+  PRIMARY KEY (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `manifestations`
+--
+
+CREATE TABLE IF NOT EXISTS `manifestations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` date NOT NULL,
+  `placesDisponibles` int(11) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `prixAdherent` double(2,2) NOT NULL,
+  `prixExterieur` double(2,2) NOT NULL,
+  `prixEnfant` double(2,2) NOT NULL,
+  `dateCreation` date NOT NULL,
+  `idAssociation` int(11) NOT NULL,
+  `idAlbum` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membres`
+--
+
+CREATE TABLE IF NOT EXISTS `membres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(50) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `idAutorisation` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `idMembre` int(11) NOT NULL,
+  `idSujet` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `newsletters`
+--
+
+CREATE TABLE IF NOT EXISTS `newsletters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `fichier` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photos`
+--
+
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `idAlbum` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photosdemandees`
+--
+
+CREATE TABLE IF NOT EXISTS `photosdemandees` (
+  `idPhoto` int(11) NOT NULL,
+  `idDemande` int(11) NOT NULL,
+  PRIMARY KEY (`idPhoto`,`idDemande`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projets`
+--
+
+CREATE TABLE IF NOT EXISTS `projets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `photo` varchar(50) NOT NULL,
+  `dateEstimation` date NOT NULL,
+  `avancement` int(11) NOT NULL,
+  `financement` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `propositionsidees`
+--
+
+CREATE TABLE IF NOT EXISTS `propositionsidees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(1500) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `reponse` varchar(1500) NOT NULL,
+  `idMembre` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `propositionssondages`
+--
+
+CREATE TABLE IF NOT EXISTS `propositionssondages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `proposition` varchar(50) NOT NULL,
+  `idQuestion` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(150) NOT NULL,
+  `idType` int(11) NOT NULL,
+  `idSondage` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `responsessondages`
+--
+
+CREATE TABLE IF NOT EXISTS `responsessondages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(50) NOT NULL,
+  `idQuestion` int(11) NOT NULL,
+  `idProposition` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sondages`
+--
+
+CREATE TABLE IF NOT EXISTS `sondages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) NOT NULL,
+  `actif` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sujets`
+--
+
+CREATE TABLE IF NOT EXISTS `sujets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `idMembre` int(11) NOT NULL,
+  `idCategorieForum` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `typequestions`
+--
+
+CREATE TABLE IF NOT EXISTS `typequestions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
