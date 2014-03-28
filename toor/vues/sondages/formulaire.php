799628e29050cba212c0b185c68231adc09bf4bf
alexandrewavelet <wavelet.alexandre@gmail.com>
2014-03-28 18:10:44 +0100

    Fix de la position des labels

--- toor/vues/sondages/formulaire.php
+++ toor/vues/sondages/formulaire.php
@@ -8,26 +8,28 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<form action="cGestionSondages.php?action=valider" method="POST" class="form-horizontal">
+			<form action="cGestionSondages.php?action=valider" method="POST" class="form-horizontal" role="form">
 				<div class="row">
 					<?php
 						echo '<input type="hidden" name="titre" id="titre" value="'.$titre.'" />';
 						echo '<input type="hidden" name="nbQuestions" id="nbQuestions" value="'.$nbQuestions.'" />';
 						for ($i=0; $i < $nbQuestions; $i++) {
 							$num = $i + 1;
-							echo '<div class="col-sm-6">';
+							echo '<div class="col-md-6">';
 							echo '<fieldset><legend>Question '.$num.'</legend>';
 							echo '<div class="form-group">
-								<label for="question'.$num.'">Question</label>
+								<label for="question'.$num.'" class="col-sm-3" >Question</label>
 								<div class="col-sm-6"><input type="text" name="question'.$num.'" id="question'.$num.'" class="form-control" /></div>
 							</div>';
 							echo '<div class="form-group">
+								<div class="col-sm-9 pull-right">
 								<label class="radio-inline"><input name="type'.$num.'" type="radio" id="1" value="1">Case à cocher</label>
 								<label class="radio-inline"><input name="type'.$num.'" type="radio" id="2" value="2">Liste déroulante</label>
+								</div>
 							</div>';
 							for ($j=1; $j < 6; $j++) { 
 								echo '<div class="form-group">
-									<label for="proposition'.$j.'-'.$num.'">Réponse '.$j.'</label>
+									<label for="proposition'.$j.'-'.$num.'" class="col-sm-3" >Réponse '.$j.'</label>
 									<div class="col-sm-6"><input type="text" name="proposition'.$j.'-'.$num.'" id="proposition'.$j.'-'.$num.'" class="form-control" /></div>
 								</div>';								
 							}
