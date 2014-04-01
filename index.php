4e370dbf7bc3aca34180e648c8fa7c59f1081db2
helalnour <helal.nour@hotmail.fr>
2014-04-01 21:46:04 +0200

    Test structure accueil 1

--- index.php
+++ index.php
@@ -81,16 +81,26 @@
   </div><!-- /.container-fluid -->
 </nav>
 
-<div class="row">
-				<div class="col-md-8">&nbsp;</div>
-				<div class="col-md-4">
-					<?php include('includes/afficherSondage.php'); ?>
-				</div>
-			</div>
-		</div>
 
-<form class="form-horizontal" role="form">
-	  <a href="inscription.php">Inscrivez vous ici</a>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Présentation</legend>
+    Texte
+  </div>
+</div>
+
+
+
+<div class="row">
+  <div class="col-md-4">
+
+
+
+<div class="panel panel-default">
+  <div class="panel-body">
+  
+    <legend>Connectez vous</legend>
   <div class="form-group">
 
     <div class="col-md-2">
@@ -106,21 +116,108 @@
   <div class="form-group">
     <div class="col-md-2 col-sm-10">
       <div class="checkbox">
-   
+
 
       </div>
     </div>
   </div>
+      <a href="inscription.php">Inscrivez vous ici</a>
   <div class="form-group">
-    <div class="col-md-2 col-sm-10">
+    <div class="col-md-10 col-sm-10">
       <button type="submit" class="btn btn-default">Valider</button>
     </div>
   </div>
+  </div>
+</div>
 
-</form>
+
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Sondage</legend>
+<div class="row">
+        <div class="col-md-10">&nbsp;</div>
+        <div class="col-md-3">
+          <?php include('includes/afficherSondage.php'); ?>
+        </div>
+      </div>
+    </div>
+      </div>
 
 
 
+
+
+
+
+
+
+
+<div class="row">
+  <div class="col-md-4">
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Informations</legend>
+    Texte personnalisé
+  </div>
+</div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Evènement</legend>
+    Prochain évènement 
+  </div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="row">
+  <div class="col-md-4">
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Newletter</legend>
+    <div class="checkbox">
+    <label>
+      <input type="checkbox"> Je m'inscris à la Newsletter
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Valider</button>
+</div>
+</div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+<div class="panel panel-default">
+  <div class="panel-body">
+    <legend>Pub</legend>
+<img src="http://lorempixel.com/output/fashion-h-c-64-98-4.jpg" alt="img1" />   </div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<img src="http://lorempixel.com/output/fashion-h-c-64-98-4.jpg" alt="img1" /> 
+<img src="http://lorempixel.com/output/nocturne-q-c-130-120-6.jpg" alt="img1" /> 
+
+   <div align="center">
+    Mentions légales - Noms des divers groupes, ect ect 
+  
+   </div>
 
 	</body>	
 </html>
