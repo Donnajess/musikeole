47bfeeb3385f6c1c45f0effdc7c917ffff6e406f
alexandrewavelet <wavelet.alexandre@gmail.com>
2014-04-06 10:09:55 +0200

    Affichage manifestations passées et en attente

--- toor/vues/agenda/index.php
+++ toor/vues/agenda/index.php
@@ -60,33 +60,61 @@
 		</div>
 	</div>
 
-	<div class="row">
-		<div class="col-md-12">
-			<h2>Manifestations en attente</h2>
-			<div class="panel-group" id="accordionAttente">
-				<?php
-					if (count($manifestationsEnAttente) > 0) {
+	<?php
+		if (count($manifestationsEnAttente) > 0) {
+			echo '<div class="row">';
+				echo '<div class="col-md-12">';
+					echo '<h2>Manifestations en attente</h2>';
+					echo '<div class="panel-group" id="accordionAttente">';
 						foreach ($manifestationsEnAttente as $manif) {
 							echo '<div class="panel panel-default">';
 								echo '<div class="panel-heading">';
-									echo '<h4 class="panel-title"><a href="#collapse'.$manif->nomFormate().'" data-parent="#accordionHistorique" data-toggle="collapse">'.$manif->getNom().'</a></h4>';
+									echo '<h4 class="panel-title"><a href="#collapse'.$manif->nomFormate().'" data-parent="#accordionEnAtttente" data-toggle="collapse">'.$manif->getNom().'</a></h4>';
 								echo '</div>';
 								echo '<div id="collapse'.$manif->nomFormate().'" class="panel-collapse collapse">';
 									echo '<div class="panel-body">';
-										echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum repudiandae fugiat eligendi ratione. Quidem, veniam, eum, illum in molestias eos dignissimos hic qui sed blanditiis neque dolore quo omnis deserunt?</p>';
-										echo '<button class="btn btn-primary">Valider la manifestation</button>';
-										echo '<button class="btn btn-danger pull-right">Supprimer la manifestation</button>';
+										echo '<div class="row">';
+											echo '<div class="col-md-6">';
+												echo $manif->getDescription();
+												echo '<hr>';
+												echo '<div class="row">';
+													echo '<div class="col-md-6">';
+														echo '<p>Le '.$manif->getDate().' à '.$manif->getHeure().'</p>';
+													echo '</div>';
+													echo '<div class="col-md-6">';
+														echo '<p>Organisé par "'.$manif->getMembre()->getNom().'"</p>';
+													echo '</div>';
+												echo '</div>';
+												echo '<hr>';
+												if ($manif->getGratuit()) {
+													echo '<p>Manifestation gratuite</p>';
+												}else{
+													echo '<div class="row">';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix adhérent</strong> : '.$manif->getPrixAdherent().' Euros</p>';
+														echo '</div>';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix extérieur</strong> : '.$manif->getPrixExterieur().' Euros</p>';
+														echo '</div>';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix enfant</strong> : '.$manif->getEnfant().' Euros</p>';
+														echo '</div>';
+													echo '</div>';
+												}
+											echo '</div>';
+											echo '<div class="col-md-6">';
+												echo '<img src="../data/images/manifestations/'.$manif->getImage().'" class="img-responsive">';
+											echo '</div>';
+										echo '</div>';
 									echo '</div>';
 								echo '</div>';
 							echo '</div>';
-						}	
-					}else{
-						echo '<p>Aucune manifestation en attente pour le moment.</p>';
-					}
-				?>
-			</div>
-		</div>
-	</div>
+						}
+					echo '</div>';
+				echo '</div>';
+			echo '</div>';
+		}
+	?>
 
 	<div class="row">
 		<div class="col-md-12">
@@ -129,9 +157,76 @@
 								echo '</div>';
 								echo '<div id="collapse'.$manif->nomFormate().'" class="panel-collapse collapse">';
 									echo '<div class="panel-body">';
-										echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum repudiandae fugiat eligendi ratione. Quidem, veniam, eum, illum in molestias eos dignissimos hic qui sed blanditiis neque dolore quo omnis deserunt?</p>';
-										echo '<button class="btn btn-primary">Ajouter l\'album photo</button>';
-										echo '<button class="btn btn-danger pull-right">Supprimer la manifestation</button>';
+										echo '<div class="row">';
+											echo '<div class="col-md-6">';
+												echo $manif->getDescription();
+												echo '<hr>';
+												echo '<div class="row">';
+													echo '<div class="col-md-6">';
+														echo '<p>Le '.$manif->getDate().' à '.$manif->getHeure().'</p>';
+													echo '</div>';
+													echo '<div class="col-md-6">';
+														echo '<p>Organisé par "'.$manif->getAssociation()->getNom().'"</p>';
+													echo '</div>';
+												echo '</div>';
+												echo '<hr>';
+												if ($manif->getGratuit()) {
+													echo '<p>Manifestation gratuite</p>';
+												}else{
+													echo '<div class="row">';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix adhérent</strong> : '.$manif->getPrixAdherent().' Euros</p>';
+														echo '</div>';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix extérieur</strong> : '.$manif->getPrixExterieur().' Euros</p>';
+														echo '</div>';
+														echo '<div class="col-sm-4">';
+															echo '<p><strong>Prix enfant</strong> : '.$manif->getEnfant().' Euros</p>';
+														echo '</div>';
+													echo '</div>';
+												}
+												echo '<hr>';
+												echo '<div class="row">';
+													echo '<div class="col-md-6">';
+														echo '<p><strong>Album photo</strong></p>';
+													echo '</div>';
+													echo '<div class="col-md-6">';
+														if (null !== $manif->getAlbum()) {
+															echo $manif->getAlbum()->getNom();
+															// + 4 photos aléatoires
+														} else {
+															echo '<p>Aucun</p>';
+														}
+													echo '</div>';
+												echo '</div>';
+											echo '</div>';
+											echo '<div class="col-md-6">';
+												echo '<img src="../data/images/manifestations/'.$manif->getImage().'" class="img-responsive">';
+											echo '</div>';
+										echo '</div>';
+										echo '<div class="row">';
+											echo '<div class="col-md-8">';
+												echo '<form action="cGestionAgenda.php?action=ajouterAlbum" method="POST" class="form-horizontal">';
+													echo '<div class="form-group">';
+														echo '<label for="album" class="control-label col-md-2">Album photo</label>';
+														echo '<div class="col-md-6">';
+															echo '<select class="form-control" required>';
+																echo '<option></option>';
+																echo '<option>Lorem ipsum</option>';
+																echo '<option>dolor sit amet.</option>';
+																echo '<option>consectetur</option>';
+															echo '</select>';
+														echo '</div>';
+														echo '<div class="col-md-3">';
+															echo '<button type="submit" class="btn btn-primary btn-agenda">Ajouter l\'album photo</button>';
+														echo '</div>';
+													echo '</div>';
+												echo '</form>';
+											echo '</div>';
+											echo '<div class="col-md-3 col-md-offset-1">';
+												echo '<button type="button" class="btn btn-danger btn-agenda-supprimer"><a href="cGestionAgenda.php?action=supprimerManifestation&id='.$manif->getId().'">Supprimer la manifestation</a></button>';
+											echo '</div>';
+										echo '</div>';
 									echo '</div>';
 								echo '</div>';
 							echo '</div>';
