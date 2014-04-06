e89e496a90616c24fce0eeccf3b13b9c8422bc04
alexandrewavelet <wavelet.alexandre@gmail.com>
2014-04-06 15:03:30 +0200

    fix affichage pub

--- includes/pubs.php
+++ includes/pubs.php
@@ -2,7 +2,7 @@
 
 	include('modeles/contenus/Publicite.php');
 	$pdo = new ConnexionBDD();
-	$reqPubs = $pdo->getConnexion()->prepare('SELECT * FROM publicites WHERE active = 1');
+	$reqPubs = $pdo->getConnexion()->prepare('SELECT * FROM publicites WHERE active = 1 ORDER BY indice DESC');
 	$reqPubs->execute();
 	$listePubs = array();
 	while ($ligne = $reqPubs->fetch()) {
