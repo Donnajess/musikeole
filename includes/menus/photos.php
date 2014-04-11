aceebefd5ba2ebd27324e0d08c5b9df1b6cf762c
alexandrewavelet <wavelet.alexandre@gmail.com>
2014-04-11 18:35:18 +0200

    Menu latéral album + CSS

--- includes/menus/photos.php
+++ includes/menus/photos.php
@@ -1,12 +1,17 @@
 <?php
 	if ($action == 'album') {
+		if ($album->getManifestation()->getAssociation()->getId() == 0) {
+			$lien = 'href="cPresentation.php"';
+		}else{
+			$lien = 'href="cPresentation.php?action=association&id='.$album->getManifestation()->getAssociation()->getId().'"';
+		}
 ?>
 <div class="list-group">
 	<a <?php echo 'href="cAgenda.php?action=detailManifestation&id='.$album->getManifestation()->getId().'"'; ?> class="list-group-item">
 		<h4 class="list-group-item-heading"><?php echo $album->getManifestation()->getNom(); ?></h4>
 		<p class="list-group-item-text">Manifestation</p>
 	</a>
-	<a <?php echo 'href="cPresentation.php?action=association&id='.$album->getManifestation()->getAssociation()->getId().'"'; ?> class="list-group-item">
+	<a <?php echo $lien; ?> class="list-group-item">
 		<h4 class="list-group-item-heading"><?php echo $album->getManifestation()->getAssociation()->getNom(); ?></h4>
 		<p class="list-group-item-text">Association</p>
 	</a>
